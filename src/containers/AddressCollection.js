3b5bd489c2f4f29b2732de0f97bd626f6cf5bfc7
Nicky Dover <ndover@bonobos.com>
2022-10-30 14:57:43 -0400

    pass last name

--- src/containers/AddressCollection.js
+++ src/containers/AddressCollection.js
@@ -1,21 +1,44 @@
 import SaveTheDateImage from "../components/SaveTheDateImage";
-import { createSearchParams, Link, useNavigate } from "react-router-dom";
+import {
+  createSearchParams,
+  Link,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+import { useState } from "react";
 
 const AddressCollection = () => {
   const navigate = useNavigate();
-  const params = { test: "hello" };
-  const goToForm = () =>
+  const [searchParams] = useSearchParams();
+  const initialLastName = searchParams.get("lastName") || "";
+  const [lastName, setLastName] = useState(initialLastName);
+  const params = { lastName };
+  const goToForm = (e) => {
+    e.preventDefault();
     navigate({
       pathname: "address-collection-form",
       search: `?${createSearchParams(params)}`,
     });
+  };
   return (
     <>
       <h1>Let us know how to reach you!</h1>
       <SaveTheDateImage />
       <p>Kirsten & Nicky</p>
       <p>Would like to know how to reach you!</p>
-      <button onClick={goToForm}>Enter Your Information!</button>
+      <form onSubmit={goToForm}>
+        <label>
+          Your Last Name:
+          <input
+            type="text"
+            aria-label="Last Name"
+            aria-required="true"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Let us know how to reach you!" />
+      </form>
     </>
   );
 };
