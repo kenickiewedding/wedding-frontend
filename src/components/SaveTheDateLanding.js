bfac708ed51b38067a4144485a07dd8ae078b7fa
nickyEXE <dovern42@gmail.com>
2022-11-28 21:57:41 -0500

    add address to landing

--- src/components/SaveTheDateLanding.js
+++ src/components/SaveTheDateLanding.js
@@ -7,7 +7,8 @@ const SaveTheDateLanding = ({ openForm }) => (
       alt="Nicky and Kirsten under a willow tree"
     />
     <p id="date">April 1, 2023</p>
-    <p id="address">New York, NY</p>
+    <p>Hill Country Barbecue Market</p>
+    <p>30 W 26th St, New York, NY 10010</p>
     <button onClick={openForm}>Share Your Contact Info!</button>
   </div>
 );
