0b512ad24f3f7f6a9fa86bf176643da37e96084e
Nicky Dover <ndover@bonobos.com>
2022-12-05 01:23:27 -0500

    properly behaving mobile navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const { pathname } = useLocation();
-  const [showNav, setShowNav] = useState("false");
+  const [showNav, setShowNav] = useState(false);
   const className = showNav ? "show navbar" : "navbar";
+  const toggleNav = () => setShowNav(!showNav);
   return (
-    <div className={className}>
-      <Link exact to="/" className={pathname === "/" && "active"}>
-        Home
-      </Link>
-      <NavLink to="/save-the-date">Share Your Contact Info</NavLink>
-      <NavLink to="/FAQ">FAQ</NavLink>
-      <NavLink to="/Travel">Travel</NavLink>
+    <div className="navbar-container">
+      <div className={className}>
+        <Link exact to="/" className={pathname === "/" && "active"}>
+          Home
+        </Link>
+        <NavLink to="/save-the-date">Share Your Contact Info</NavLink>
+        <NavLink to="/FAQ">FAQ</NavLink>
+        <NavLink to="/Travel">Travel</NavLink>
+      </div>
+      <FontAwesomeIcon icon={faBars} onClick={toggleNav} />
     </div>
   );
 };
