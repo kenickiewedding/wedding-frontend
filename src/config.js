7a60d7c2af98568be8cecbdba47210d749db42f7
nickyEXE <dovern42@gmail.com>
2023-01-21 00:59:02 -0500

    handle between dev and prod

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const API = "https://nicky-and-kirsten-wedding.herokuapp.com";
+export const API =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://nicky-and-kirsten-wedding.herokuapp.com";
