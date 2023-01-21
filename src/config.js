66fa244e55bc1f9ca2cbc1435452fe14ca544fd8
nickyEXE <dovern42@gmail.com>
2023-01-21 03:37:35 -0500

    fix endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-export const API =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://nicky-and-kirsten-wedding.herokuapp.com";
+export const API = "https://nicky-and-kirsten-wedding.herokuapp.com";
