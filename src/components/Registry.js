a7d514524db6e03c655956dc339740f6739c052a
Nicky Dover <ndover@bonobos.com>
2023-01-30 17:38:44 -0500

    add target link and copy

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -1,4 +1,24 @@
 const Registry = () => {
-  return <h1>Registry</h1>;
+  return (
+    <div className="registry">
+      <h3>Registry</h3>
+      <p className="question">
+        We’ve happily built a home over the last six years and celebrating
+        together will be the greatest gift of all.
+      </p>
+      <p className="question">
+        If you’d like to give us a gift, we’re registered at the sites below:
+      </p>
+
+      <button
+        onClick={() =>
+          (window.location.href =
+            "https://www.target.com/gift-registry/gift/kirsten-and-nicky-do-a-wedding")
+        }
+      >
+        Target
+      </button>
+    </div>
+  );
 };
 export default Registry;
