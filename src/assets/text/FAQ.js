88ffca794677c3618d30f268c04003b723eaf0ec
nickyEXE <dovern42@gmail.com>
2023-02-26 19:54:02 -0500

    Update FAQ copy

--- src/assets/text/FAQ.js
+++ src/assets/text/FAQ.js
@@ -4,61 +4,56 @@ export const FAQuestions = [
     question:
       "This wedding is happening on April 1st. Is it actually happening?",
     answerParagraphs: [
-      "Yes! Notice that you’re reading this website and it probably isn’t April 1st, so you can probably trust the information on it!",
-    ],
+      "Yes! Notice that you’re reading this website and it probably isn’t April 1st, so you can probably trust the information on it!"
+    ]
   },
   {
     id: 2,
     question:
       "I have a hilarious idea for a practical joke to play on the bride and groom at the wedding. Can I do it?",
     answerParagraphs: [
-      "The Best Man, Diana Dover, will also be the Czar of Practical Jokes. Please consult with her before doing anything drastic.",
-    ],
+      "The Best Man, Diana Dover, will also be the Czar of Practical Jokes. Please consult with her before doing anything drastic."
+    ]
   },
   {
     id: 3,
     question: "When will I receive my invitation?",
     answerParagraphs: [
-      "You’ll receive your formal invitation and information about how to RSVP in early January!",
-    ],
+      "You’ll receive your formal invitation and information about how to RSVP in early January!"
+    ]
   },
   {
     id: 4,
     question: "Where are the ceremony and reception being held?",
     answerParagraphs: [
       "Both the ceremony and reception will be held at the same location, Hill Country Barbecue Market, located at 30 W. 26th Street in the Flatiron District of Manhattan.",
-      "The venue is a two-level space, and has an elevator for those who need it.",
-    ],
+      "The venue is a two-level space, and has an elevator for those who need it."
+    ]
   },
   {
     id: 5,
     question: "What should I wear?",
     answerParagraphs: [
-      "Attire is festive cocktail. New York City can still be a little chilly in early April, so we’ll have a place for you to deposit your stylish overcoat when you arrive at the venue. ",
-    ],
+      "Attire is festive cocktail. New York City can still be a little chilly in early April, so we’ll have a place for you to deposit your stylish overcoat when you arrive at the venue. "
+    ]
   },
   {
     id: 6,
     question:
       "What COVID precautions are being taken to keep everyone safe and healthy?",
     answerParagraphs: [
-      "We are excited to celebrate our wedding with our beloved family and friends (that’s you!).",
-      "That group includes people who are older, immunocompromised, or otherwise at-risk, as well as people with brand new tiny babies at home.",
-      "Please help us protect our loved ones by adhering to the following safety guidelines.",
-      "We will be requiring that all guests are fully vaccinated with all recommended COVID vaccines and boosters.",
-      "As of right now that means you’ll need to have received the latest bivalent booster. You will also need to take a rapid test and share the results with our wedding coordinator just before arriving at the venue.",
-      "More information will be provided in the lead-up to the event. We appreciate your cooperation!",
-      "As for other illnesses, we won’t be testing for those, but common sense still applies!",
-      "If you come down with flu, the Red Death, or are bleeding from the eyes after seeing someone else bleeding from the eyes on your flight, please feel free to cancel and let us know.",
-    ],
+      "We are excited to celebrate our wedding with our beloved family and friends (that’s you!). That group includes people who are older, immunocompromised, or otherwise at-risk, as well as people with brand new tiny babies at home.",
+      "We will be requiring that all guests are fully vaccinated with all recommended COVID vaccines and boosters. As of right now that means you’ll need to have received the latest bivalent booster. You will also need to take a rapid test and share the results with our wedding coordinator just before arriving at the venue.",
+      "More information will be provided in the lead-up to the event. We appreciate your cooperation! As for other illnesses, we won’t be testing for those, but common sense still applies! If you come down with flu, the Red Death, or are bleeding from the eyes after seeing someone else bleeding from the eyes on your flight, please feel free to cancel and let us know."
+    ]
   },
   {
     id: 7,
     question:
       "I can’t make it in person, but still want to celebrate on your wedding day!",
     answerParagraphs: [
-      "We will be providing a simple live broadcast of the wedding ceremony so you can still participate virtually. More information to come in early 2023.",
-    ],
+      "We will be providing a simple live broadcast of the wedding ceremony so you can still participate virtually. More information to come in early 2023."
+    ]
   },
   {
     id: 8,
@@ -68,9 +63,9 @@ export const FAQuestions = [
       {
         string: "Kirsten’s former museum intern Ben Eshleman! We are obsessed.",
         linkText: "Ben Eshleman",
-        href: "https://www.beneshleman.com/",
-      },
-    ],
+        href: "https://www.beneshleman.com/"
+      }
+    ]
   },
   {
     id: 9,
@@ -81,8 +76,8 @@ export const FAQuestions = [
       {
         string: "kirstenandnickyplanawedding@gmail.com.",
         linkText: "kirstenandnickyplanawedding@gmail.com",
-        href: "mailto:kirstenandnickyplanawedding@gmail.com.",
-      },
-    ],
-  },
+        href: "mailto:kirstenandnickyplanawedding@gmail.com."
+      }
+    ]
+  }
 ];
--- src/assets/text/travel.js
+++ src/assets/text/travel.js
@@ -5,8 +5,8 @@ export const travelQuestions = [
     answerParagraphs: [
       "Practice.",
       "We typically travel through LaGuardia Airport, but John F. Kennedy International Airport and Newark Liberty International Airport are also good options.",
-      "If you’re coming from the East Coast, the venue is also located close to Penn Station and the shiny new (and irritatingly benchless) Moynihan Train Hall.",
-    ],
+      "If you’re coming from the East Coast, the venue is also located close to Penn Station and the shiny new (and irritatingly benchless) Moynihan Train Hall."
+    ]
   },
   {
     id: 2,
@@ -15,14 +15,21 @@ export const travelQuestions = [
       "New York City has some of the best public transportation in the world! It’s safe and easy to navigate the city by subway and bus.",
       "With the new OMNY system, you can pay at the subway turnstile or as you board the bus by tapping a credit card. Google Maps is a reliable source for finding the most efficient route to anywhere you want to go.",
       "Taxi cabs are also plentiful! We recommend downloading the Curb app, which functions like Lyft or Uber but with NYC’s iconic yellow taxis.",
-      "Curb will also allow you to pair your payment with a ride already in progress, allowing you to do the thing people in New York movies do where they pull up to a place in a taxi and immediately get out of the car.",
-    ],
+      "Curb will also allow you to pair your payment with a ride already in progress, allowing you to do the thing people in New York movies do where they pull up to a place in a taxi and immediately get out of the car."
+    ]
   },
   {
     id: 3,
     question: "Where should I stay?",
     answerParagraphs: [
-      "There won’t be a specific wedding hotel block reserved. There are tons of hotel options at various price points right near our venue in the Flatiron District neighborhood in Manhattan. We’ll update this page with a few suggestions in January.",
-    ],
-  },
+      "We have no official wedding blocks at any hotel, but there are a LOT of hotels near the venue to suit your individual needs.",
+      {
+        string:
+          "To see a list of hotels within a 15-minute walk of the venue, along with their prices and perks, click here.",
+        linkText: "click here.",
+        href: "https://www.google.com/travel/hotels/Hill%20Country%20Barbecue%20Market%2C%2030%20W%2026th%20St?q=hotels%20near%20Hill%20country%20barbecue%20ny&g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4270442%2C4284970%2C4291517%2C4306835%2C4429192%2C4515404%2C4597339%2C4731329%2C4757164%2C4778035%2C4810789%2C4810791%2C4814050%2C4861688%2C4864715%2C4874190%2C4886082%2C4886480%2C4893075%2C4902277%2C4906019%2C4920132%2C4924069%2C4926165%2C4926489%2C4934306%2C4936396%2C4946041%2C4955788%2C4960757%2C4961150%2C4962401&hl=en-US&gl=us&ssta=1&ts=CAESBgoCCAMQARogCgIaABIaEhQKBwjnDxAEGAESBwjnDxAEGAIYATICCAEqCQoFOgNVU0QaAA&rp=ogEqSGlsbCBDb3VudHJ5IEJhcmJlY3VlIE1hcmtldCwgMzAgVyAyNnRoIFN0OAFAAEgCmgICCAA&ap=SAAwAVqvAgoFCN4CEAAiA1VTRCoWCgcI5w8QBBgBEgcI5w8QBBgCGAEgAbABAVgBcgQIAhgAogEVCgkvbS8wMl8yODYSCE5ldyBZb3JrqgEKCgIIEhICCGkYAaoBEgoCCBQSAgg_EgIIcBICCB0YAaoBGwoCCBwSAwiXARICCFESAghzEgIIRxICCDYYAaoBEwoCCCUSAwigAhICCHgSAgh5GAGqARIKAggREgIIQBICCDgSAgh_GAGqATYKAgguEgIIPBICCFYSAwiAAhICCDoSAwiHARICCEgSAwiBARIDCIMBEgIIAxICCFMSAggoGAGqAQwKAwiuARIDCLMBGAGqAQoKAgg1EgIIRBgBkgICCA2SAgIIDpICAggSkgICCAySAgIIE5ICAggPkgICCBSSAQIgAWgA&ictx=1&utm_campaign=sharing&utm_medium=link&utm_source=htls&ved=0CAAQ5JsGahcKEwi4gePavLT9AhUAAAAAHQAAAAAQBA"
+      },
+      "If you need help navigating the options, feel free to reach out to Kirsten or Nicky."
+    ]
+  }
 ];
