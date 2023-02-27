d536223254a8ded94218e7b272666e34fec12539
nickyEXE <dovern42@gmail.com>
2023-02-26 20:26:05 -0500

    change registry

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -2,15 +2,15 @@ const Registry = () => {
   return (
     <div className="registry">
       <h3></h3>
-      <p className="question">
+      <p>
         We’ve happily built a home together over the last six years and
         celebrating together will be the greatest gift of all, so please don't
         feel obligated to give us a gift.
       </p>
-      <p className="question">
-        If you’d like to give us a gift, we’re registered{" "}
+      <p>
+        If you’d like to give us a gift, we’re registered {" "}
         <a href="https://www.target.com/gift-registry/gift/kirsten-and-nicky-do-a-wedding">
-          at Target here.
+          here.
         </a>
       </p>
     </div>
