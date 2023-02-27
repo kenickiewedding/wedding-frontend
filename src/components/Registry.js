4b58ba38d47ed29337e71407e6268a7b03d450ca
nickyEXE <dovern42@gmail.com>
2023-02-26 20:21:52 -0500

    change registry stuff

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -1,23 +1,18 @@
 const Registry = () => {
   return (
     <div className="registry">
-      <h3>Registry</h3>
+      <h3></h3>
       <p className="question">
-        We’ve happily built a home over the last six years and celebrating
-        together will be the greatest gift of all.
+        We’ve happily built a home together over the last six years and
+        celebrating together will be the greatest gift of all, so please don't
+        feel obligated to give us a gift.
       </p>
       <p className="question">
-        If you’d like to give us a gift, we’re registered at the sites below:
+        If you’d like to give us a gift, we’re registered{" "}
+        <a href="https://www.target.com/gift-registry/gift/kirsten-and-nicky-do-a-wedding">
+          at Target here.
+        </a>
       </p>
-
-      <button
-        onClick={() =>
-          (window.location.href =
-            "https://www.target.com/gift-registry/gift/kirsten-and-nicky-do-a-wedding")
-        }
-      >
-        Target
-      </button>
     </div>
   );
 };
